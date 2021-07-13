c7243fcf68b8c664d785ea990d7ffdadefb08395
Julian Rojas <jfrojas251@gmail.com>
2021-07-13 00:25:35 -0500

    Dateinputs and dropdown tests

--- bpop-hipotecario-lib-ds/src/molecules/date-inputs/date-inputs.test.tsx
+++ bpop-hipotecario-lib-ds/src/molecules/date-inputs/date-inputs.test.tsx
@@ -6,12 +6,31 @@ afterEach(cleanup);
 
 test("DateInputs should renders ok", async () => {
   const x = render(<DateInputs caption="This is a caption" id="date-inputs-birthday" onChange={() => null} />);
-  const dayInput = await x.container.getElementsByTagName("input")[0];
-  const yearInput = await x.container.getElementsByTagName("input")[1];
-  const firstDropdownItem = await x.container.getElementsByClassName("item")[0];
+  const dayInput = x.container.getElementsByTagName("input")[0];
+  const yearInput = x.container.getElementsByTagName("input")[1];
+  const firstDropdownItem = x.container.getElementsByClassName("item")[0];
 
   fireEvent.change(dayInput, { target: { value: 1 } });
   fireEvent.change(yearInput, { target: { value: 1997 } });
   fireEvent.click(firstDropdownItem);
   expect(screen.getByText("Day")).toBeInTheDocument();
+});
+
+test("DateInputs day and year emits null", async () => {
+  let result;
+  const expectedResult = { day: 0, month: 0, year: 0 };
+  const x = render(<DateInputs caption="This is a caption" id="date-inputs-birthday" onChange={(v) => (result = v)} />);
+  const dayInput = x.container.getElementsByTagName("input")[0];
+  const yearInput = x.container.getElementsByTagName("input")[1];
+
+  fireEvent.change(dayInput, { target: { value: 1 } });
+  fireEvent.change(yearInput, { target: { value: 1997 } });
+  fireEvent.change(dayInput, { target: { value: null } });
+  fireEvent.change(yearInput, { target: { value: null } });
+  expect(expectedResult).toEqual(result);
+});
+
+test("DateInputs should has caption with error", async () => {
+  render(<DateInputs caption="This is a caption" id="date-inputs-birthday" hasError={true} onChange={() => null} />);
+  expect(screen.getByText("This is a caption")).toHaveClass("error");
 });
