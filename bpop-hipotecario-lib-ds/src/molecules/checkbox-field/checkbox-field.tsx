5feefcab5684653c757c46917cd9715233c5dd8d
David Granados <david.granados@avaldigitallabs.com>
2021-08-09 15:37:21 -0500

    :fix: add onBlur to dropdown

--- bpop-hipotecario-lib-ds/src/molecules/checkbox-field/checkbox-field.tsx
+++ bpop-hipotecario-lib-ds/src/molecules/checkbox-field/checkbox-field.tsx
@@ -6,7 +6,7 @@ import { getId, getSuffixedId } from "../../helpers";
 
 import styles from "./checkbox-field.module.css";
 
-const CheckboxField: React.FC<CheckboxFieldProps> = ({ children, dataTestId, id = "checkbox", ...props }) => {
+const CheckboxField: React.FC<CheckboxFieldProps> = ({ children, dataTestId, id = "checkbox-field", ...props }) => {
   return (
     <div {...getSuffixedId(id, "wrapper", dataTestId)} className={styles["wrapper"]}>
       <Checkbox {...getId(id, dataTestId, true)} {...props} />
